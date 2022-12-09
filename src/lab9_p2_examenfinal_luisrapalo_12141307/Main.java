760c5e7ff7c83c0b858f4233275e6797db62135f
LRapalo <luisrapalo987@gmail.com>
2022-12-09 17:57:05 -0600

    me voy

--- src/lab9_p2_examenfinal_luisrapalo_12141307/Main.java
+++ src/lab9_p2_examenfinal_luisrapalo_12141307/Main.java
@@ -38,6 +38,7 @@ public class Main extends javax.swing.JFrame {
             System.out.println();
         }
     }
+    //lols
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
